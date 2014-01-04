72b21e676d02c146e15fd091211b88d08a9848e9
Kyle <kyle@realbigmarketing.com>
2014-01-03 22:13:50 -0500

    Lots of work done!
    A prototype is in progress. I think I know how I am now going to
    categorize the short codes and present. Learned a lot about arrays.

--- admin/admin.php
+++ admin/admin.php
@@ -4,87 +4,90 @@ This would be a great file for adding code that you want run in the backend. For
 create an admin or a settings page, this would be the appropriate place to work.
 */
 
-//put your code here
-
-add_action("admin_menu","starter_menu_fcn");
-
-function starter_menu_fcn()
+if(is_admin())
 {
-	add_menu_page("PageTitle","MenuTitle","manage_options","starter-plugin.php", "menu_body_fcn");
+	// Create the Paulund toolbar
+	$shortcodes = new View_All_Available_Shortcodes();
 }
 
-function menu_body_fcn()
+/**
+ * View all available shrotcodes on an admin page
+ *
+ * @author
+ **/
+class View_All_Available_Shortcodes
 {
-/*	
- * THE FOLLOWING WILL YIELD A SIMPLE TABLE THAT HOUSES A RADIO ARRAY THAT WILL ALLOW THE SELECTION OF VALUES
- * FOR THE OPTION var1
- * THE OPTION VALUE IS SET/CREATED BY THE UPDATE_OPTION FUNCTION WHICH CREATES OPTIONS IF THEY DON'T EXIST
- * THE OPTIONS ARE FRAMED IN A FORM THAT CAN BE SUBMITTED WITH A SUBMIT BUTTON 
- * THE SAVE OPTIONS PORTION OF THE FEATURE IS ACTIVATED BY THE isset($_POST["submitOptions"]) CLAUSE 
- * 
- */
-	if(isset($_POST["submitOptions"])) //Catches the fact that the submit button has been pressed
+	public function __construct()
 	{
-		//by using implode we can encode the selected checkboxes into a single comma delimited string.
-		//You should look out for the possibility of the values having unescaped commas as this could cause problems.
-		update_option("checkboxVar",implode(",",$_POST["checkboxArray"]));
-		update_option("radioVar",$_POST["option1Arr"]);
-		update_option("textoption", $_POST["textinput"]);
+		$this->Admin();
 	}
-	//since there is an array of values we will need an array of elements to see which checkboxes should be checked
-	$checkboxarr = explode(",",	get_option("checkboxVar"));
-	?>
-	<!-- The checkboxes and the radio buttons both use the "checked" tag to indicate that the element is active. 
-		The conditional operator in the following form is used to determine if the element is selected
-			print (ISSELECTED?"checked":""); 
-		this is a condensed "If" statment and ensures that the option selected in the database shows checked 
-		in the table.  
-		-->
-	<div>
-		Plugin Version : <?php print get_option("starter_plugin_version"); ?> (<b>Value created when plugin activated</b>)
-		
-		<form name="option-form" method="post">
-		<!-- This table is going to house an array of checkboxes. The table is not important but the [] in the name of each element tells the form to handle them as an array -->
-		</br>
-		<table>
-			<tr><th> Value </th><th>Checkbox</th></tr>
-			<tr>
-				<td>First Option</td>
-				<td><input type="checkbox" name="checkboxArray[]" value="First Value" <?php print (in_array("First Value", $checkboxarr)?"checked":"");?> /></td>
-			</tr>
-			<tr>
-				<td>Second Option</td>
-				<td><input type="checkbox" name="checkboxArray[]" value="Second Value" <?php print (in_array("Second Value", $checkboxarr)?"checked":"");?> /></td>
-			</tr>
-			<tr>
-				<td>Third Option</td>
-				<td><input type="checkbox" name="checkboxArray[]" value="Third Value" <?php print (in_array("Third Value", $checkboxarr)?"checked":"");?> /></td>
-			</tr>
-		</table>
-		<!-- This table holds the radio buttons, once again the table is not important but the radio buttons will need
-			to have the same name so that they will be part of the same array of radio buttons -->
-		</br>
-		<table>
-			<tr><th> Value </th><th>Radio</th></tr>
-			<tr>
-				<td>Val 1 </td>
-				<td><input type="radio" name="option1Arr" value="Val1" <?php print (get_option("radioVar")=="Val1"?"checked":"");?> /></td>
-			</tr>
-			<tr>
-				<td>Val 2 </td>
-				<td><input type="radio" name="option1Arr" value="Val2" <?php print (get_option("radioVar")=="Val2"?"checked":"");?> /></td>
-			</tr>
-		</table>
-		
-		<!-- This textbox could in principle also be a textarea for a large blob of text -->
-		</br>
-		<b>Text Value</b> </br>
-		<input type="text" name="textinput" value="<?php print get_option("textoption"); ?>"/>
-		
-		</br>
-		<input type="submit" name="submitOptions" />
-		</form>
-	</div>
-	<?php
-}
+	/**
+	 * Create the admin area
+	 */
+	public function Admin(){
+		add_action( 'admin_menu', array(&$this,'Admin_Menu') );
+	}
+
+	/**
+	 * Function for the admin menu to create a menu item in the settings tree
+	 */
+	public function Admin_Menu(){
+		add_submenu_page(
+			'options-general.php',
+			'View All Shortcodes',
+			'View All Shortcodes',
+			'manage_options',
+			'view-all-shortcodes',
+			array(&$this,'Display_Admin_Page'));
+	}
+
+	/**
+	 * Display the admin page
+	 */
+	public function Display_Admin_Page(){
+		global $shortcode_tags;
+
+        ?>
+        <div class="wrap">
+        	<div id="icon-options-general" class="icon32"><br /></div>
+			<h2>View All Available Shortcodes</h2>
+			<div class="section panel">
+				<p>This page will display all of the available shortcodes that you can use on your WordPress blog.</p>
+        	<table class="widefat importers">
+        		<tr><td><strong>Shortcodes</strong></td></tr>
+        <?php
+
+	        foreach($shortcode_tags as $code => $function)
+	        {
+	        	?>
+	        		<tr><td>[<?php echo $code; ?>]</td></tr>
+	        	<?php
+	        }
+	    ?>
+
+			</table>
+			<table class="widefat importers">
+				<tr><td>
+					<h1>Using a foreach loop with our multidimensional array</h1>
+					<ol>
+				<?php global $kjm_cats;
+					for ($row = 0; $row < 99; $row++) {
+								echo "<li> Row #$row";
+								echo "<ul>";
+								foreach($kjm_cats[$row] as $key => $value) {
+									echo "<li>".$key.$value."</li>";
+								}
+								echo "</ul>";
+								echo "</li>";
+
+								}
+					?>
+					</ol>
+				</td></tr>
+			</table>
+			</div>
+		</div>
+		<?php
+	}
+} // END class View_All_Available_Shortcodes
 ?>
--- ultimate-shortcodes-library.php
+++ ultimate-shortcodes-library.php
@@ -13,9 +13,43 @@ When I use snippets from other developers I generally leave a link here back to 
 them credit for being awesome and also as a reference for myself later. Not required but recommended.
 */
 
+$kjm_cats = array(
+	array(
+		'Holiday'=>'Christmas',
+		'Month'=>'December',
+		'Day'=>25,
+		'Season'=>'Winter'
+		),
+	array(
+		'Holiday'=>'Halloween',
+		'Month'=>'October',
+		'Day'=>31,
+		'Season'=>'Fall'
+		),
+	array(
+		'Holiday'=>'New Years Day',
+		'Month'=>'January',
+		'Day'=>1,
+		'Season'=>'Winter'
+		)
+	);
+
+/*This is what I'd have in every shortcode file
+for adding the shortcodes created there
+to the master array*/
+$kjm_add = array(
+		'Holiday'=>'Independence Day',
+		'Month'=>'July',
+		'Day'=>4,
+		'Season'=>'Summer'
+		);
+
+$kjm_cats[]=$kjm_add;
+
 //Generally I begin with a main plugin file (which would be this) and use it to assemble all the others together.
 //It is good practice to segment your code into different files. This is a good technique for including them:
 require_once (plugin_dir_path(__FILE__).'/admin/admin.php');
 require_once (plugin_dir_path(__FILE__).'/shortcodes/all.php');
+
 
 ?>
