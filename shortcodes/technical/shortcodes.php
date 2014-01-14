9b112bc4c57582bd28d7a116e9f303f696dd9fa6
Kyle <kyle@realbigmarketing.com>
2014-01-13 21:53:37 -0500

    Moved this to the holiday shortcodes
    usl_days_until

--- shortcodes/technical/shortcodes.php
+++ shortcodes/technical/shortcodes.php
@@ -18,29 +18,5 @@ $usl_codes[] = array(
 		'Description'=>'Outputs the current month.',
 		'Category'=>'Technical'
 		);
-/*-------------------------------
-Number of days until...
--------------------------------*/
-function usl_days_until($atts, $content = null){
-  extract(shortcode_atts(array(
-     'month' => '',
-     'day'   => '',
-     'year'  => ''
-    ), $atts));
-    $remain = ceil((mktime( 0,0,0,(int)$month,(int)$day,(int)$year) - time())/86400);
-    if( $remain >= 1 ){
-        return $daysremain = "$remain";
-    } else {
-        return $content;
-    }
-}
-add_shortcode('usl_days_until', 'usl_days_until');
-$usl_codes[] = array(
-		'Title'=>'Number of days until specific date',
-		'Code'=>'usl_days_until',
-		'Atts'=>'day, month, year',
-		'Description'=>'Outputs the number of days until a specified date.',
-		'Example'=>'[usl_days_until month="2" day="14" year="2014"]',
-		'Category'=>'Technical'
-		);
+
 ?>
