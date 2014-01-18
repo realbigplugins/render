70eeaa7a9cb306cfc2d7bd408b4b65bd6b85af66
Kyle <kyle@realbigmarketing.com>
2014-01-18 13:10:05 -0500

    Finishing up usl_button
    Styling mostly.

--- shortcodes/design/shortcodes.php
+++ shortcodes/design/shortcodes.php
@@ -11,21 +11,22 @@ function usl_button($atts, $content = null) {
 	global $usl_add_style;
 	$usl_add_style = true;
 	extract(shortcode_atts(array(
-    'link' => '',
+    'link' => '#',
     'size'   => 'small',
-    'color'  => 'red'
+    'color'  => 'red',
+    'shape' => 'rounded'
     ), $atts));
 
-    return "<a href='$link' class='usl-$color usl-$size'>".$content."</a>";
+    return "<a href='$link' class='usl-$color usl-$size usl-$shape'>".$content."</a>";
 }
 add_shortcode('usl_button', 'usl_button');
 
 $usl_button = array(
 		'Title'=>'Button',
 		'Code'=>'usl_button',
-		'Atts'=>'link, color(red, blue), size(large, medium, small)',
+		'Atts'=>'link, color(red, blue, green, orange), size(large, medium, small), shape(square, rounded, round)',
 		'Description'=>'Creates a sweet button',
-		'Example'=>'[usl_button link="#" size="large" color="blue"]Click here[/usl_button]',
+		'Example'=>'[usl_button link="#" size="large" color="blue" shape="round"]Click here[/usl_button]',
 		'Category'=>'Design'
 		);
 $usl_codes[]=$usl_button;
