44bbf732caa475aa2b08ca2ed7faf2f525654a50
Kyle Maurer <kyle@realbigmarketing.com>
2014-12-31 18:09:10 -0500

    Creating a query shortcode. Functional so far. Just needs more $atts for tweaking results and options for modifying the display.

---
+++ src/core/shortcodes/core/query.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * Contains all Render packaged shortcodes within the Query category.
+ *
+ * @since      Render 1.0.0
+ *
+ * @package    Render
+ * @subpackage Shortcodes
+ */
+
+$_shortcodes = array(
+	// Post meta
+	array(
+		'code'        => 'render_query',
+		'function'    => '_render_query',
+		'title'       => __( 'Query', 'Render' ),
+		'description' => __( 'Queries the database for posts.', 'Render' ),
+		'tags'        => 'query database posts data loop',
+		'atts'        => array(
+			'author' => array(
+				'label' => __( 'Author', 'Render' ),
+				'type'        => 'selectbox',
+				'properties'  => array(
+					'placeholder'      => __( 'Author', 'Render' ),
+					'options'          => 'test',
+				),
+			),
+			'post_type' => array(
+				'label'       => __( 'Post type', 'Render' ),
+				'type'        => 'selectbox',
+				'description' => __( 'Available post types.', 'Render' ),
+				'properties'  => array(
+					'placeholder'      => __( 'Post Type', 'Render' ),
+					'options'          => get_post_types(),
+				),
+			),
+		),
+		'render'      => true,
+		'wrapping' => false
+	)
+);
+
+foreach ( $_shortcodes as $shortcode ) {
+	$shortcode['category'] = 'query';
+	$shortcode['source']   = 'Render';
+	render_add_shortcode( $shortcode );
+}
+
+/**
+ * Runs a WP_Query.
+ *
+ * @since  0.3.0
+ * @access Private
+ *
+ * @param null|array $atts The attributes sent to the shortcode.
+ *
+ * @return string
+ */
+function _render_query( $atts ) {
+
+	$atts = shortcode_atts( array(
+		'post_type' => '',
+		'author' => '',
+	), $atts );
+
+	// Escape atts
+	render_esc_atts( $atts );
+
+	$args = array(
+		'post_type' => $atts['post_type'],
+		'author' => $atts['author']
+	);
+
+	$output = '';
+	$query = new WP_Query( $args );
+
+	if ( $query->have_posts() ) {
+		$output = '<ul>';
+		while ( $query->have_posts() )  {
+			$query->the_post();
+			$output .= '<li>' . get_the_title() . '</li>';
+		}
+		$output .= '</ul>';
+	} else {
+		$output = 'No posts found';
+	}
+	return $output;
+}
