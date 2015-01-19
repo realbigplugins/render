b1611c3213b3dd30ac89c44ee8158d1e60690c09
joelworsham <joelworsham@gmail.com>
2015-01-19 14:07:21 -0500

    Resolves #66
    I think that should do it, but we'll need to test it

--- src/core/admin/settings.php
+++ src/core/admin/settings.php
@@ -9,9 +9,9 @@ if ( ! defined( 'ABSPATH' ) ) {
  *
  * Provides the admin page for adjusting Render settings.
  *
- * @since 1.0.0
+ * @since      1.0.0
  *
- * @package Render
+ * @package    Render
  * @subpackage Admin
  */
 class Render_AdminPage_Settings extends Render {
@@ -44,6 +44,10 @@ class Render_AdminPage_Settings extends Render {
 	public static function register_settings() {
 
 		register_setting( 'render_options', 'render_render_visual' );
+
+		// EDD Licensing
+		register_setting( 'render_options', 'edd_sample_license_key', 'edd_sanitize_license' );
+
 	}
 
 	public static function body_class( $classes ) {
@@ -64,6 +68,8 @@ class Render_AdminPage_Settings extends Render {
 		}
 
 		$render_visual = get_option( 'render_render_visual', '1' );
+		$license       = get_option( 'edd_sample_license_key' );
+		$status        = get_option( 'edd_sample_license_status' );
 		require( ABSPATH . 'wp-admin/options-head.php' );
 		?>
 		<div class="wrap render-wrap">
@@ -72,11 +78,41 @@ class Render_AdminPage_Settings extends Render {
 				<?php _e( 'Settings', 'Render' ); ?>
 			</h2>
 
-			<form method="post" action="settings.php">
+			<form method="post" action="options.php">
 
 				<?php settings_fields( 'render_options' ); ?>
 
 				<table class="render-table">
+					<tr valign="top">
+						<th scope="row" valign="top">
+							<?php _e( 'License Key', 'Render' ); ?>
+						</th>
+						<td>
+							<label>
+								<input id="edd_sample_license_key" name="edd_sample_license_key" type="text"
+								       class="regular-text" value="<?php esc_attr_e( $license ); ?>"/>
+							</label>
+						</td>
+					</tr>
+					<?php if ( ! empty( $license ) ) { ?>
+						<tr valign="top">
+							<th scope="row" valign="top">
+								<?php _e( 'Activate License', 'Render' ); ?>
+							</th>
+							<td>
+								<?php if ( $status !== false && $status == 'valid' ) { ?>
+									<span style="color:green;"><?php _e( 'active', 'Render' ); ?></span>
+									<?php wp_nonce_field( 'edd_sample_nonce', 'edd_sample_nonce' ); ?>
+									<input type="submit" class="button-secondary" name="edd_license_deactivate"
+									       value="<?php _e( 'Deactivate License', 'Render' ); ?>"/>
+								<?php } else {
+									wp_nonce_field( 'edd_sample_nonce', 'edd_sample_nonce' ); ?>
+									<input type="submit" class="button-secondary" name="edd_license_activate"
+									       value="<?php _e( 'Activate License', 'Render' ); ?>"/>
+								<?php } ?>
+							</td>
+						</tr>
+					<?php } ?>
 					<tr>
 						<th scope="row">
 							<?php _e( 'Use the magical visual renderer?', 'Render' ); ?>
@@ -101,3 +137,12 @@ class Render_AdminPage_Settings extends Render {
 }
 
 new Render_AdminPage_Settings();
+
+function edd_sanitize_license( $new ) {
+	$old = get_option( 'edd_sample_license_key' );
+	if ( $old && $old != $new ) {
+		delete_option( 'edd_sample_license_status' ); // new license has been entered, so must reactivate
+	}
+
+	return $new;
+}
